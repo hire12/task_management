acde8760ad67ee03b0c2ee00b9e0dcfefac6edf0
Hiriyan Mohammed <hireemoh@gmail.com>
2025-01-10 23:26:36 +0300

    some style on task list

---
+++ client/src/pages/createTask/createTask.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+export default function createTask() {
+    const [form , setForm] = useState({ title: "", description: "",  })
+  return (
+    <div>
+
+    </div>
+  )
+}
