51ca7d6ebb8cc618a0a2a52286f858c54d7510e9
Hiriyan Mohammed <hireemoh@gmail.com>
2025-01-10 23:23:33 +0300

    some style on task list

--- server/src/controller/test.ts
+++ server/src/controller/test.ts
@@ -1,30 +1,43 @@
-import { Request, Response } from "express";
-import Task from "../models/Task";
+// import { Request, Response } from "express";
+// import Task from "../models/Task";
 
-export const newTask = async (req: Request, res: Response ) => {
-    const {title, description, dueDate, priority} = req.body;
+// export const newTask = async (req: Request, res: Response ) => {
+//     const {title, description, dueDate, priority} = req.body;
 
-    try {
-        const task = new Task({
-            title,
-            description,
-            dueDate,
-            priority,
-            userId: (req as any).user.id,
-        });
-        await task.save();
-        res.status(201).json({ msg: "Task Created success" });
-    } catch(err) {
-        res.status(500).json({ mgs: "server Error", error: err })
-    }
-};
+//     try {
+//         const task = new Task({
+//             title,
+//             description,
+//             dueDate,
+//             priority,
+//             userId: (req as any).user.id,
+//         });
+//         await task.save();
+//         res.status(201).json({ msg: "Task Created success" });
+//     } catch(err) {
+//         res.status(500).json({ mgs: "server Error", error: err })
+//     }
+// };
 
 
-export const getTask = async (req: Request, res: Response) => {
-    try {
-        const task = Task.find({ userId: (req as any).user.id,  });
-        res.status(200).json({ task })
-    } catch(err) {
-        res.status(500).json({ msg: "server error", error: err })
-    }
-}
+// export const getTask = async (req: Request, res: Response) => {
+//     try {
+//         const task = Task.find({ userId: (req as any).user.id,  });
+//         res.status(200).json({ task })
+//     } catch(err) {
+//         res.status(500).json({ msg: "server error", error: err })
+//     }
+// }
+
+// export const getTaskById = async (req: Request, res: Response) => {
+
+//     try {
+//         const task = await Task.findOne({ _id: req.params.id, userId: (req as any).user.id  });
+//         if(!task) {
+//             res.status(404).json({ msg: "Task Not Found" })
+//         }
+//         res.status(200).json({ task })
+//     } catch(err) {
+//         res.status(500).json({ error: err })
+//     }
+// }
